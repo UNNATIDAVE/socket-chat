3febff11fc516717e374124efb0daf1eb4bfdc21
Unnati Dave <unnatidave11@gmail.com>
2018-04-25 20:25:05 +0530

    Final file

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require('./library/Chat.js').sockets(http);
 
 //db connection
 mongoose.Promise = global.Promise;
-var dbPath = "mongodb://localhost/chatDatabase";
+var dbPath = "mongodb://socketchat:socketapi@ds157599.mlab.com:57599/socket-chat";
 mongoose.connect(dbPath,{ useMongoClient: true });
 mongoose.connection.once('open',function(){
   console.log("Database Connected Successfully.");
